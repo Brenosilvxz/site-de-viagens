53119ac18b6f4c766c5620ea8f9c9137c9983d3b
Brenosilvxz <josephbreno72@gmail.com>
2026-08-28 16:26:16 -0300

    feat: adionando header e suas informações.

--- rota-certa/src/Components/Header/index.jsx
+++ rota-certa/src/Components/Header/index.jsx
@@ -1,10 +1,40 @@
-import Noronha from "../../assets/fernando-de-noronha.jpg"
-import "./Header.css"
+import Noronha from "../../assets/fernando-de-noronha.jpg";
+import "./Header.css";
 
 export default function Header() {
   return (
     <div className="Back">
-        <img src={Noronha} className="img" alt="" />
+
+      {/* IMAGEM */}
+      <img
+        src={Noronha}
+        className="img"
+        alt="Fernando de Noronha"
+      />
+
+      {/* CAMADA ESCURA */}
+      <div className="overlay"></div>
+
+      {/* CONTEÚDO */}
+      <div className="info-icon">◎</div>
+
+      <span>Sua próxima história começa aqui</span>
+
+      <h1>
+        Viaje para sentir o mundo de verdade.
+      </h1>
+
+      <p>
+        Roteiros autorais, lugares inesquecíveis e experiências
+        que aproximam você da cultura local.
+      </p>
+        
+      <span>Fernando de Noronha</span>
+
+      <h2>
+        Mergulhe em águas que parecem irreais.
+      </h2>
+
     </div>
-  )
+  );
 }
