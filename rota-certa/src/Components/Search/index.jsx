d1dfc8105186704d9a24d2db3c3b7a9e9b927216
Brenosilvxz <josephbreno72@gmail.com>
2026-09-02 17:21:14 -0300

    refactor: editando a NavBar e adicionando função a barra de pesquisa

--- rota-certa/src/Components/Search/index.jsx
+++ rota-certa/src/Components/Search/index.jsx
@@ -1,19 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./Search.css";
 
 export default function Search() {
+  const [destino, setDestino] = useState("");
+
+  const [data, setData] = useState("");
+
+  const navigate = useNavigate();
+
+  function pesquisar() {
+    if (!destino) {
+      alert("Digite um destino");
+
+      return;
+    }
+
+    navigate(`/destinos?busca=${destino}&data=${data}`);
+  }
+
   return (
     <div className="search">
       <div className="campo">
         <small>PARA ONDE?</small>
-        <input placeholder="Descubra um destino" />
+
+        <input
+          placeholder="Descubra um destino"
+          value={destino}
+          onChange={(e) => setDestino(e.target.value)}
+        />
       </div>
 
       <div className="campo data">
         <small>QUANDO?</small>
-        <input placeholder="Escolha a data" />
+
+        <input
+          type="date"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
       </div>
 
-      <button>⌕</button>
+      <button onClick={pesquisar}>🔍</button>
     </div>
   );
 }
